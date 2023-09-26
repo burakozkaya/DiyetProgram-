69a33fc1b825de68a579c745b16813ecbc7e3d81
Burak Özkaya <ozkayaburak97@gmail.com>
2023-09-26 09:30:12 +0300

    appConfig added ConfigurationManager added

---
+++ DiyetProgramı.DAL/Context/DiyetProgramıDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace DiyetProgramı.DAL.Context
+{
+    public class DiyetProgramıDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString);
+        }
+    }
+}
