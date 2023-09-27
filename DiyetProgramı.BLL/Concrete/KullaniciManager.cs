4cf43a5419d7354bb06a5875afc0381ef5414f79
Burak Özkaya <ozkayaburak97@gmail.com>
2023-09-27 11:40:30 +0300

    BLL updated

---
+++ DiyetProgramı.BLL/Concrete/KullaniciManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using DiyetProgramı.DAL.Concrete;
+using DiyetProgramı.Entities.Concrete;
+
+namespace DiyetProgramı.BLL.Concrete
+{
+    public class KullaniciManager : BaseManager<Kullanici>
+    {
+        private KullaniciRepo _repo;
+        public KullaniciManager(BaseRepo<Kullanici> baseRepo) : base(baseRepo)
+        {
+            _repo = (KullaniciRepo?)baseRepo;
+        }
+
+        public bool UserLogin(Kullanici user)
+        {
+            return _repo.UserLogin(user);
+        }
+
+        public bool ValidUser(string username)
+        {
+            return _repo.ValidUser(username);
+        }
+
+        public bool ValidMail(string username)
+        {
+            string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
+            return Regex.IsMatch(username, pattern);
+        }
+
+        public bool ValidPassword(string password)
+        {
+            bool tempLower = false;
+            bool tempUpper = false;
+            bool tempSpec = false;
+            foreach (var VARIABLE in password)
+            {
+                if (Char.IsLower(VARIABLE))
+                    tempLower = true;
+                else if (Char.IsUpper(VARIABLE))
+                    tempUpper = true;
+                else if (Char.IsSymbol(VARIABLE) || Char.IsPunctuation(VARIABLE))
+                    tempSpec = true;
+            }
+
+            if (tempSpec && tempUpper && tempLower)
+                return true;
+            return false;
+        }
+    }
+}
